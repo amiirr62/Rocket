889dac0a92284d7d75909b471b7584fd5985b8dd
Amir <amiirr62@yahoo.com>
2023-02-28 20:39:37 -0800

    Implementing setConfig

--- app/index.js
+++ app/index.js
@@ -3,6 +3,13 @@ const app = express()
 const http = require('http')
 const path = require('path')
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
+const mongoose = require('mongoose')
+const { body, validationResult } = require('express-validator')
+const flash = require('connect-flash')
+const passport = require('passport')
 
 
 module.exports = class Application {
@@ -21,12 +28,28 @@ module.exports = class Application {
         }
 
         setConfig(){
-            app.use(express.static('public'))
+            mongoose.connect('mongodb://127.0.0.1:27017/NJS-Khodam').then(() => console.log('Connected!'))
+            mongoose.set('strictQuery', false)
+
+            app.use(express.static(__dirname + '/public'))
             app.set('view engine', 'ejs')
             app.set('views', path.resolve('./resource/views'))
 
             app.use(bodyParser.json())
             app.use(bodyParser.urlencoded({extended : true}))
+            
+            app.use(cookieParser('hbdakq2eq2q5546535qopkosqnwx9849'))
+            app.use(session({
+                secret: 'mysecrettkkey',
+                resave: true,
+                saveUninitialized: true,
+                cookie : {expires : new Date(Date.now() + (1000 * 3600 * 24 * 100)) ,
+                          store   : MongoStore.create({ mongoUrl: 'mongodb://127.0.0.1:27017/Rocket' })
+                        }
+              }))
+            app.use(flash()) 
+
+
 
             app.get('/', (req,res)=>{
                 res.json('Hello Rocket')
