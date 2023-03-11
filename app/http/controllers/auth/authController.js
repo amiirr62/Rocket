b53fe53a7e04170c48adbe6e7f02e81bf3b07cc7
Amir <amiirr62@yahoo.com>
2023-03-11 13:05:40 -0800

    Recaptcha Implemented for /register

--- app/http/controllers/auth/authController.js
+++ app/http/controllers/auth/authController.js
@@ -1,16 +1,21 @@
 const express = require('express')
 const { body, validationResult } = require('express-validator')
+const Controller = require('../Controller')
+
+const Recaptcha = require('express-recaptcha').RecaptchaV2
+
+var options = { hl: 'en' }
+var recaptcha = new Recaptcha('6LcIXvIkAAAAAA26fU0fC9KSEkwhJjcCk5XhcDcd', 
+                              '6LcIXvIkAAAAAP05uPm9nFyJhzHfrOf9n0hl2kcK', options)
 
 
 
-
-
-class authController  {
+class authController extends Controller  {
      
     
     async showRegisterForm(req,res,next){
         try {   
-             return res.render('auth/register')
+             return res.render('auth/register',{recaptcha : recaptcha.render()})
             } catch (err) {
                 next(err)
             }
@@ -20,22 +25,45 @@ class authController  {
     async showLoginForm(req,res,next){
         try {
             
-            return  res.render('auth/login')
+            return  res.render('auth/login',{recaptcha : recaptcha.render()})
         } catch (err) {
             next(err)
         }
     }
     
 
-    registerProcess(req,res,next){
-       
+    async  registerProcess(req,res,next){
+       try{
+//************************** Recaptcha Result Process ************************ */
+        let recaptchaResult =  await new Promise((resolve,reject)=>{
+            recaptcha.verify(req,(err,data)=>{
+                if(err){
+                    req.flash('errors','Checkmark Recaptcha plz !!!')
+                    res.redirect(req.url)
+                    resolve(false)
+                }else{
+                    resolve(true)
+                }
+
+            })
+        })
+
+        if(!recaptchaResult){
+            return 
+        }
+//************************** End OF Recaptcha Result Process ************************ */
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
         let myErrors = (errors.array())
         req.flash('errors', myErrors)
         
         return res.redirect('/register')
-    }
+
+       }
+    }catch (err) {
+        next(err)
+}
+
 
         
     return res.redirect('/login')
